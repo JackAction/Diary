0f27745fae1fc2f4694fba9369f752224a57ea1a
Fabian <JackAction>
2017-02-15 10:02:56 +0100

    UC Base als Basis für alle UC Tabs. Diary erbt von Base. Restliche UC noch nicht.

---
+++ Diary/MainForm/Views/ucBase.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MainForm
+{
+    partial class ucBase
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(476, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // ucBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Name = "ucBase";
+            this.Size = new System.Drawing.Size(905, 558);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
--- Diary/MainForm/Views/ucDiary.cs
+++ Diary/MainForm/Views/ucDiary.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace MainForm
 {
-    public partial class ucDiary : UserControl
+    public partial class ucDiary : ucBase
     {
         public ucDiary()
         {
